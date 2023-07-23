86d3c74d544768fe9f512ef6fdece9f018fed780
RainPPR <PPR2125773894@163.com>
2023-07-23 17:00:56 +0800

    20230723

--- set_git.bat
+++ set_git.bat
@@ -1,8 +1,5 @@
 @echo off
-
 git config --global http.sslverify false
 git config --global user.name "RainPPR"
 git config --global user.email "PPR2125773894@163.com"
 git config --global color.ui auto
-git init
-git remote add origin https://github.com/RainPPR/My-VSCode.git
