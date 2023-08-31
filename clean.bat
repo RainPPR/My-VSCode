37806e4bf90ac15ab7c886405ed88729129e24fd
RainPPR <PPR2125773894@163.com>
2023-08-31 17:47:53 +0800

    娓呯悊鏂囦欢澶?

--- clean.bat
+++ clean.bat
@@ -1,5 +1,24 @@
 @echo off
+setlocal
+
+for /f "usebackq" %%a in (`dir /s /a /w /b ^| find /v /c ""`) do set "size_before=%%a"
+
 git push --force --verbose --dry-run
 git push --force
+
+git add .
+git commit -m "清理文件夹"
+git push -u origin master -f
+
 git reflog expire --expire=now --all
 git gc --prune=now
+
+for /f "usebackq" %%a in (`dir /s /a /w /b ^| find /v /c ""`) do set "size_after=%%a"
+
+set /a size_difference=size_after-size_before
+
+echo.
+echo The folder size has changed by %size_difference% bytes.
+
+endlocal
+pause
