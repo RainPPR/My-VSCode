f5a9cc66fd30b65815ad821468d95d291bb000a3
RainPPR <PPR2125773894@163.com>
2023-09-03 12:49:01 +0800

    20230903

--- 加速读写.cpp
+++ 加速读写.cpp
@@ -1,17 +1,34 @@
 #include <bits/stdc++.h>
 
+#define rr read()
+#define rl readline()
+
+#define ww(x) write(x)
+#define wl(x) writeline(x)
+
 using namespace std;
 
 inline int read()
 {
     int num = 0, flag = 1;
-    int ch = getchar();
+    char ch = getchar();
     for (; !isdigit(ch); ch = getchar())
         if (ch == '-')
             flag = -1;
     for (; isdigit(ch); ch = getchar())
         num = (num << 3) + (num << 1) + ch - '0';
     return num * flag;
+}
+
+inline string readline()
+{
+    string str;
+    char ch = getchar();
+    while (ch <= 13)
+        ch = getchar();
+    while (ch > 13)
+        str += ch, ch = getchar();
+    return str;
 }
 
 inline void write(int x)
@@ -31,14 +48,23 @@ inline void write(int x)
     putchar('\n');
 }
 
+inline void writeline(string str)
+{
+    while (str.back() <= 13)
+        str.pop_back();
+    for (char ch : str)
+        putchar(ch);
+    putchar('\n');
+}
+
 int main()
 {
     freopen("copycat.in", "r", stdin);
     freopen("copycat.out", "w", stdout);
 
-    int n = read();
+    int n = rr;
     while (n--)
-        write(read());
+        wl(rl);
 
     fclose(stdin);
     fclose(stdout);
