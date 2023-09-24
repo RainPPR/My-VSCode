1c15583c012762fffda7967502847294e6837831
RainPPR <PPR2125773894@163.com>
2023-09-24 13:29:29 +0800

    20230924

---
+++ 模板.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+
+#define gor(_Tp, x, s, t) for (_Tp x = s; x < t; ++x)
+#define gore(_Tp, x, s, t) for (_Tp x = s; x <= t; ++x)
+#define rgor(_Tp, x, s, t) for (_Tp x = s; x > t; --x)
+#define rgore(_Tp, x, s, t) for (_Tp x = s; x >= t; --x)
+#define _gor(_Tp, x, s, t, p) for (_Tp x = s; x < t; x += p)
+#define _gore(_Tp, x, s, t, p) for (_Tp x = s; x <= t; x += p)
+#define _rgor(_Tp, x, s, t, p) for (_Tp x = s; x > t; x -= p)
+#define _rgore(_Tp, x, s, t, p) for (_Tp x = s; x >= t; x -= p)
+
+#define debug(x) cerr << #x << " = " << x << endl
+#define meow(args...) fprintf(stderr, args)
+
+#define rr read()
+#define ur uread()
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef __int128 int128;
+typedef unsigned __int128 uint128;
+
+template<typename _Tp> _Tp _max(const _Tp a, const _Tp b) { return a > b ? a : b; }
+template<typename _Tp> _Tp _min(const _Tp a, const _Tp b) { return a < b ? a : b; }
+template<typename _Tp> _Tp _abs(const _Tp x) { return x < 0 ? -x : x; }
+
+signed read() {
+    signed num = 0, flag = 1; char ch = getchar();
+    for (; !isdigit(ch); ch = getchar()) if (ch == '-') flag = -1;
+    for (; isdigit(ch); ch = getchar()) num = num * 10 + ch - '0';
+    return num * flag;
+}
+
+unsigned uread() {
+    unsigned num = 0; char ch = getchar();
+    while(!isdigit(ch)) ch = getchar();
+    while(isdigit(ch)) num = num * 10 + ch - '0', ch = getchar();
+    return num;
+}
+
+signed main() {
+    // freopen("xxx.in", "r", stdin);
+    // freopen("xxx.out", "w", stdout);
+    int n = ur;
+    // fclose(stdin);
+    // fclose(stdout);
+    return 0;
+}
--- ！测试/test1.cpp
+++ ！测试/test1.cpp
@@ -1,26 +1,18 @@
 #include <bits/stdc++.h>
+#include <cstdint>
 
 using namespace std;
 
-const int N = 30;
+#ifdef __SIZEOF_INT128__
+    #define int128 __int128
+    #define uint128 unsigned __int128
+#else
+    #define int128 __int128_t
+    #define uint128 __uint128_t
+#endif
 
-struct matrix {
-    int n, m, a[N][N];
-    matrix operator *(const matrix t) const {
-        matrix r = {n, t.m, {0}};
-        for (int i = 0; i < n; ++i) for (int j = 0; j < t.m; ++j)
-            for (int k = 0; k < m; ++k) r.a[i][j] = (r.a[i][j] + a[i][k] * t.a[k][j]) & 1;
-        return r;
-    }
-};
-
-int main()
-{
-    matrix t = {3, 3, {1, 0}};
-    cout << t.n << endl;
-    cout << t.m << endl;
-    cout << t.a[0][0] << endl;
-    cout << t.a[0][1] << endl;
-    cout << t.a[1][2] << endl;
+signed main() {
+    _INT128_DEFINED
+    uint128 a = 3;
     return 0;
 }
