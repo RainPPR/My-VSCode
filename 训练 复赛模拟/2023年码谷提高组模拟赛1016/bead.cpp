36c257b8eb939cd7564265f7cd957bd77915d4fb
RainPPR <PPR2125773894@163.com>
2023-10-17 22:10:21 +0800

    20231017

--- 训练 复赛模拟/2023年码谷提高组模拟赛1016/bead.cpp
+++ 训练 复赛模拟/2023年码谷提高组模拟赛1016/bead.cpp
@@ -1,48 +1,12 @@
 #include <bits/stdc++.h>
-
 using namespace std;
-
-#define inwin 0
-
-#define ur uread()
-inline int uread() {
-    int num = 0, ch = getchar();
-    while (!isdigit(ch)) ch = getchar();
-    while (isdigit(ch)) num = num * 10 + ch - '0', ch = getchar();
-    return num;
-}
-
-#define rl readline()
-inline string readline() {
-    string str; int ch = getchar();
-    #ifdef inwin
-    while (ch == '\n' || ch == '\r') ch = getchar();
-    while (ch != '\n' && ch != '\r') str.push_back(ch), ch = getchar();
-    #else
-    while (ch == '\n') ch = getchar();
-    while (ch != '\n') str.push_back(ch), ch = getchar();
-    #endif
-    return str;
-}
-
-const int N = 2e5 + 10;
-
-string str[N];
-
-string tominstr(string str) {
-    string str2 = str; reverse(str2.begin(), str2.end());
-    if (str < str2) return str;
-    else return str2;
-}
-
-inline bool cmp(const string a, const string b) {
-    return (a + b) < (b + a);
-}
-
+inline bool cmp(const string a, const string b) { return a + b < b + a; }
 signed main() {
-    int n = ur; for (int i = 1; i <= n; ++i) str[i] = rl;
-    sort(str + 1, str + 1 + n, cmp);
-    for (int i = 1; i <= n; ++i) printf("%s", str[i].c_str());
-    putchar('\n');
+    vector<string> str; str.clear();
+    int n; cin >> n;
+    string tmp; for (int i = 1; i <= n; ++i) cin >> tmp, str.push_back(tmp);
+    sort(str.begin(), str.end(), cmp);
+    string res; for (string i : str) res += i;
+    cout << res << endl;
     return 0;
 }
--- 训练 复赛模拟/2023年码谷提高组模拟赛1016/gate.cpp
+++ 训练 复赛模拟/2023年码谷提高组模拟赛1016/gate.cpp
@@ -1,31 +1,14 @@
 #include <bits/stdc++.h>
-
 using namespace std;
-
-#define inwin 0
-
-#define rr uread()
-inline int uread() {
-    int num = 0, ch = getchar();
-    while (!isdigit(ch)) ch = getchar();
-    while (isdigit(ch)) num = num * 10 + ch - '0', ch = getchar();
-    return num;
-}
-
 const int N = 1e5 + 10;
-
-int n, k;
+int pos1, n, k, v, dp[N], ans;
 vector<int> g[N];
-
-int dp[N], ans;
 void dfs(int u, int fa) {
     dp[u] = 1; for (int v : g[u]) dfs(v, u), dp[u] = max(dp[u], dp[v] + 1);
-    if (dp[u] > k || (dp[u] == k && fa != 1)) ++ans, dp[u] = 0;
-}
-
-signed main() {
-    n = rr, k = rr;
-    int pos1 = rr; for (int i = 2; i <= n; ++i) g[rr].push_back(i);
+    if ((u != 1) && (dp[u] > k || (dp[u] == k && fa != 1))) ++ans, dp[u] = 0;
+} signed main() {
+    cin >> n >> k >> pos1;
+    for (int i = 2; i <= n; ++i) cin >> v, g[v].push_back(i);
     dfs(1, -1); printf("%d\n", ans + (pos1 != 1));
     return 0;
 }
