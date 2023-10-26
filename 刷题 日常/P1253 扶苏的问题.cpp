c9a349f84d76dbfd400d83b3a85f3d8f3d35e92f
RainPPR <PPR2125773894@163.com>
2023-10-26 13:10:18 +0800

    20231026

--- 刷题 日常/P1253 扶苏的问题.cpp
+++ 刷题 日常/P1253 扶苏的问题.cpp
@@ -1,3 +1,6 @@
+// 一个比较强的线段树板子题
+// P1253 扶苏的问题
+
 #include <bits/stdc++.h>
 
 using namespace std;
@@ -12,36 +15,33 @@ inline int read() {
     return num * flag;
 }
 
-template<typename tp, tp N>
+template<typename tp, tp INIT>
 class segment {
 #define mid ((l + r) >> 1)
-#define _v vector<tp>
+#define vp vector<tp>
 private:
-    int n; _v s, mad, mre;
-    void _build(int k, int l, int r, _v &a) {
+    int n; vp s, mad, mre;
+    void _build(int k, int l, int r, vp &a) {
         if (l == r) { s[k] = a[l]; return; }
         _build(k * 2, l, mid, a), _build(k * 2 + 1, mid + 1, r, a);
         s[k] = max(s[k * 2], s[k * 2 + 1]);
+    } void _addmad(int k, tp x) {
+        s[k] += x;
+        if (mad[k] == -INIT) mad[k] = x;
+        else mad[k] += x;
+    } void _addmre(int k, tp x) {
+        s[k] = mre[k] = x, mad[k] = -INIT;
     } void _pushdown(int k) {
-        if (mre[k]) {
-            s[k * 2] = s[k * 2 + 1] = mre[k * 2] = mre[k * 2 + 1] = mre[k];
-            mad[k * 2] = mad[k * 2 + 1] = mre[k] = mad[k] = 0;
-        } else if (mad[k]) {
-            if (mre[k * 2]) mre[k * 2] += mad[k];
-            else s[k * 2] += mad[k];
-            if (mre[k * 2 + 1]) mre[k * 2 + 1] += mad[k];
-            else s[k * 2 + 1] += mad[k];
-            mad[k * 2] += mad[k], mad[k * 2 + 1] += mad[k];
-            mad[k] = 0;
-        }
+        if (mre[k] != -INIT) _addmre(k * 2, mre[k]), _addmre(k * 2 + 1, mre[k]), mre[k] = -INIT;
+        if (mad[k] != -INIT) _addmad(k * 2, mad[k]), _addmad(k * 2 + 1, mad[k]), mad[k] = -INIT;
     } void _add(int k, int l, int r, int p, int q, tp x) {
         if (r < p || l > q) return;
-        if (l >= p && r <= q) { mad[k] += x; return; }
+        if (l >= p && r <= q) { _addmad(k, x); return; }
         _pushdown(k); _add(k * 2, l, mid, p, q, x), _add(k * 2 + 1, mid + 1, r, p, q, x);
         s[k] = max(s[k * 2], s[k * 2 + 1]);
     } void _modify(int k, int l, int r, int p, int q, tp x) {
         if (r < p || l > q) return;
-        if (l >= p && r <= q) { mad[k] = 0, mre[k] = x; return; }
+        if (l >= p && r <= q) { _addmre(k, x); return; }
         _pushdown(k); _modify(k * 2, l, mid, p, q, x), _modify(k * 2 + 1, mid + 1, r, p, q, x);
         s[k] = max(s[k * 2], s[k * 2 + 1]);
     } tp _query(int k, int l, int r, int p, int q) {
@@ -52,37 +52,30 @@ private:
         return max(_query(k * 2 + 1, mid + 1, r, p, q), _query(k * 2, l, mid, p, q));
     }
 public:
-    void build(_v &a) {
-        n = (int)a.size() - 1, s = mad = mre = _v(3 * n), _build(1, 1, n, a);
-        cerr << "BUILD " << 1 << " " << n << endl;
+    void build(vp &a) {
+        n = (int)a.size() - 1, s = mad = mre = vp(3 * n, -INIT), _build(1, 1, n, a);
     } void add(int l, int r, tp x) {
-        cerr << "ADD " << l << " " << r << " : " << x << endl;
         _add(1, 1, n, l, r, x);
     } void modify(int l, int r, tp x) {
-        cerr << "MODIFY " << l << " " << r << " : " << x << endl;
         _modify(1, 1, n, l, r, x);
-    } tp query(int l, int r, int db = 1) {
-        if (db) { int t = _query(1, 1, n, l, r); cerr << "QUERY " << l << " " << r << " | " << t << endl; return t; }
-        else return _query(1, 1, n, l, r);
+    } tp query(int l, int r) {
+        return _query(1, 1, n, l, r);
     }
 #undef mid
-#undef _v
+#undef vp
 };
 
 const int N = 1e6 + 10;
-
-segment<ll, N> sg;
+const ll INF = 1e18;
 
 signed main() {
     int n = rr, q = rr; vector<ll> a(n + 1);
     for (int i = 1; i <= n; ++i) a[i] = rr;
+    segment<ll, INF> sg;
     sg.build(a); while (q--) {
-        cerr << "---------------" << endl;
         int op = rr, l = rr, r = rr;
         if (op == 1) sg.modify(l, r, rr);
         else if (op == 2) sg.add(l, r, rr);
         else printf("%lld\n", sg.query(l, r));
-        cerr << "-DEBUG-" << endl;
-        for (int i = 1; i <= n; ++i) fprintf(stderr, "%lld%c", sg.query(i, i, 0), " \n"[i == n]);
     } return 0;
 }
